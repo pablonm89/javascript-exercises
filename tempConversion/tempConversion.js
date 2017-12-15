b3dd71671ce9f87ebbe93c496467ef50f2f2dea8
Pablo <pablonm89@gmail.com>
2017-12-15 16:21:52 -0300

    Completed tempConversion exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-var ftoc = function() {
-  
+var ftoc = function(f) {
+  let fareToCel = (f - 32) / 1.8
+  return Math.round(fareToCel * 10) / 10
 }
 
-var ctof = function() {
-  
+var ctof = function(c) {
+  let celToFare = (c * 1.8) + 32
+  return Math.round(celToFare * 10) / 10
 }
 
 module.exports = {
