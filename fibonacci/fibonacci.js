a9ac67857674debe77b3244ec1a9880d35c6ac7f
Pablo <pablonm89@gmail.com>
2017-12-16 12:52:07 -0300

    Completed fibonacci exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-var fibonacci = function() {
+var fibonacci = function(n) {
+	if (n < 0) return 'OOPS'
+	if (typeof n == 'integer') return parseInt(n)
 
+	let fibonacciArray = getFibonacci(n)
+
+	return fibonacciArray.slice(-1)[0]
 }
 
 module.exports = fibonacci
+
+
+function getFibonacci(n) {
+	let array = [1, 1]
+	let newArray = []
+	//n-2 because we already have 2 elements in array
+	for(x = 0; x < n-2; x++) {
+		let sumNum = array[x] + array[x+1]
+		array.push(sumNum)
+	}
+	return array
+}
