1d0039cd86d39fd947df98a7d14cabe7d3b6c1ac
Pablo <pablonm89@gmail.com>
2017-12-18 15:30:30 -0300

    Completed snakeCase exercise

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,7 @@
-var snakeCase = function() {
-
+var snakeCase = function(str) {
+	let specialChars = /[\W_]/g
+	let stringOnlySpaces = str.replace(specialChars, ' ').replace(/\s+/g, ' ').trim().replace(/\s/g, '_').toLowerCase()
+	return stringOnlySpaces
 }
 
 module.exports = snakeCase
