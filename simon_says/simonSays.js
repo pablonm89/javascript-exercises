55d14782e7047322dbd80b2915c4b73455c26886
Pablo <pablonm89@gmail.com>
2017-12-18 11:39:00 -0300

    Completed simon_says exercise

--- simon_says/simonSays.js
+++ simon_says/simonSays.js
@@ -1,30 +1,39 @@
-function echo() {
-	
+function echo(str) {
+	return str
 }
 
-function shout() {
-	
+function shout(str) {
+	return str.toUpperCase()
 }
 
-function repeat() {
-
+function repeat(str, n=2) {
+	let newStr = str + ' '
+	return newStr.repeat(n).trim()
 }
 
-function pieceOfWord() {
-	
+function pieceOfWord(str, n) {
+	return str.slice(0, n)
 }
 
-function firstWord() {
-	
+function firstWord(str) {
+	let strArr = str.split(' ')
+	return strArr[0]
 }
 
 function capitalize(word) {
-	return word.charAt(0).toUpperCase() + word.slice(1);
+	let littleWords = ['and', 'over', 'on', 'the']
+	return (littleWords.includes(word)) ? word : word.charAt(0).toUpperCase() + word.slice(1)
 	// This function just capitalizes the word given to it, use in conjunction with titleCreator
 }
 
-function titleCreator() {
-	
+function titleCreator(str) {
+	let strArr = str.split(' ')
+	let newArray = []
+	strArr.forEach(word => {
+		newArray.push(capitalize(word))
+	})
+	let newArrayJoin = newArray.join(' ')
+	return newArrayJoin.charAt(0).toUpperCase() + newArrayJoin.slice(1)
 }
 
 module.exports = {
