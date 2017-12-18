035f01ff4221ebf968a03615b3f5463ad5356adb
Pablo <pablonm89@gmail.com>
2017-12-18 12:45:12 -0300

    Completed pigLatin Exercise

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,5 +1,24 @@
-function translate() {
-	// body...
+function translate(str) {
+	let vowels = /^[aeiou]/
+	let consonant = /^[^aeiou]/
+	let newWord = []
+
+	str.split(' ').forEach(word => {
+		if (vowels.test(word[0])) {
+			return newWord.push(word + 'ay')
+		} else if (consonant.test(word[0]) && word[1] == 'q' && word[2] == 'u') {
+			return newWord.push(word.slice(3) + word[0] + word[1] + word[2] + 'ay')
+		} else if (word[0] == 'q' && word[1] == 'u') {
+			return newWord.push(word.slice(2) + word[0] + word[1] + 'ay')
+		} else if (consonant.test(word[0]) && consonant.test(word[1]) && consonant.test(word[2])) {
+			return newWord.push(word.slice(3) + word[0] + word[1] + word[2] + 'ay')
+		} else if (consonant.test(word[0]) && consonant.test(word[1])) {
+			return newWord.push(word.slice(2) + word[0] + word[1] + 'ay')
+		} else if (consonant.test(word[0])) {
+			return newWord.push(word.slice(1) + word[0] + 'ay')
+		}
+	})
+	return newWord.join(' ')
 }
 
 
