adf545252320d363e9f0965d21aec11ae1d8dcb1
Pablo <pablonm89@gmail.com>
2017-12-19 15:09:08 -0300

    Fixed removeFromArray exercise

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,17 +1,6 @@
-var removeFromArray = function(arr) {
-  var args = Array.prototype.slice.call(arguments);
-
-  for (let i = 1; i < args.length; i++) {
-    let removal = args[i];
-    let j = 0;
-    while(arr.length > 0 && j < arr.length) {
-      if (arr[j] == removal) {
-        arr.splice(j, 1);
-      }
-      j++;
-    }
-  } // end for args
-  return arr;
+var removeFromArray = function(...args) {
+  const array = args[0]
+  return array.filter(val => !args.includes(val))
 }
 
 module.exports = removeFromArray
